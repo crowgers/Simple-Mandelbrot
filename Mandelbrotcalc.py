bd55a68e407ad117e51a0551e184ecfab572207f
Cormac Rogers <Crowgers@users.noreply.github.com>
2016-10-26 12:40:01 +0100

    Update Mandelbrotcalc.py

--- Mandelbrotcalc.py
+++ Mandelbrotcalc.py
@@ -42,19 +42,24 @@ def mandelbrot_main(x_min, x_max, y_min, y_max, grid_size, tolerance):
 
 print("This code will compute the mandelbrot set for a user defined number of points between uer defined boundaries.\n")
 print("The tolerance is the number of times a specific point will be interated on to determine if it is in the set.\n")
-print("A plot is saved to fil and then printed specifying the time of creation to avoid overwriting.\n")
+print("A plot is saved to file and then printed to screen.\n")
 print("Note that -2 -> x -> 2 & -2 -> y -> 2 encompass the set.\n")
+
 # mandelbrot_main(-2, 2, -2, 2, 1000, 100)
 # mandelbrot_main(0, .5, .5, 1.0, 1000, 100)
 # mandelbrot_main(.5, .6, .3, .4, 1000, 100)
 # mandelbrot_main(.34, .36, .50, 5.2, 1000, 100
 # mandelbrot_main(.30, .32, .56, .58, 1000, 100)
 # mandelbrot_main(.36, .38, .58, .60, 1000, 50)
+# Global_x_min, Global_x_max, Global_y_min, Global_y_max = [float(x) for x in raw_input("Enter x & y plotting boundaries (xmin, xmax, ymin, ymax: ").split()]
+# Global_Grid_size, Global_Tolerance = [int(x) for x in raw_input("Enter grid size and tolerance here (recommend 1000 100): ").split()]
+
 Global_x_min = float(input("Input x_min:"))
 Global_x_max = float(input("Input x_max:"))
 Global_y_min = float(input("Input y_min:"))
 Global_y_max = float(input("Input y_max:"))
 Global_Grid_size = int(input("Input grid_size:"))
 Global_Tolerance = int(input("Input tolerance:"))
-print("Computing set ...")
+
+print("Inputs ok. Computing set ...")
 mandelbrot_main(Global_x_min, Global_x_max, Global_y_min, Global_y_max, Global_Grid_size, Global_Tolerance)
