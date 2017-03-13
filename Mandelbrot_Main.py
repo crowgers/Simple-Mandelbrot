a319a07b7a696ab2bb27298cf2a65b64c50aada4
Cormac Rogers <Crowgers@users.noreply.github.com>
2017-03-13 23:38:39 +0000

    Format cleanup.  Vars renamed for Clarity

--- Mandelbrot_Main.py
+++ Mandelbrot_Main.py
@@ -1,6 +1,6 @@
 # Python code to compute, print and ave a png file of madnelbrot set at specified points
-# Grid_size defines how large array.  Sticking with uniform grid but if variable scaling is desired split into
-# grid_size_x & grid_size_y
+# num_grid_pts defines how large array.  Sticking with uniform grid but if variable scaling is desired split into
+# num_grid_pts_x & num_grid_pts_y
 # The tolerance is the number of times a specific point will be interated on to determine if it is in the set.
 # x_min, x_max, y_min, y_max are boundary of calculation (-2,2,-2,2 encompass the set).
 # Worth noting that functions use local variables and global variables carry "global tag"
@@ -9,35 +9,35 @@ import matplotlib.pyplot as plt
 
 
 # Plotting function creates a heatmap - Relied on by main.
-def plotting(value, tolerance, x_min, x_max, y_min, y_max):
+def _plotting(values, tolerance, x_min, x_max, y_min, y_max):
 	plt.xlabel("Real Axis")
 	plt.ylabel("Imaginary Axis")
 	plt.title("The Mandelbrot Set. Tolerance: %s" % tolerance)
 	plt.grid(True)
-	plt.imshow(value, origin="0,0", interpolation="none", extent=[x_min, x_max, y_min, y_max])
+	plt.imshow(values, origin="0,0", interpolation="none", extent=[x_min, x_max, y_min, y_max])
 	plt.savefig("MandelBrot.png", bbox_inches="tight")
 	plt.show()
 
 
 # Algorithm to compute if point is within set - Relied on by main
-def mandel_calc(point, tolerance):
+def _mandel_calc(point, tolerance):
 	z = 0
 	for i in range(1, tolerance):
 		if abs(z) > 2:
 			return i
-		z = z * z + point
+		z = z*z + point
 	return 0
 
 
-# Main function intialises & creates grid.  Dependent on mandel_calc & plotting.
-def mandelbrot_main(x_min, x_max, y_min, y_max, grid_size, tolerance):
-	real = np.linspace(x_min, x_max, grid_size)
-	imag = np.linspace(y_min, y_max, grid_size)
-	value = np.empty((grid_size, grid_size))
-	for i in range(grid_size):
-		for j in range(grid_size):
-			value[j, i] = mandel_calc(real[i] + 1j * imag[j], tolerance)
-	plotting(value, tolerance, x_min, x_max, y_min, y_max)
+# Main function intialises & creates grid.  Dependent on _mandel_calc & _plotting.
+def _mandelbrot_main(x_min, x_max, y_min, y_max, num_grid_pts, tolerance):
+	real = np.linspace(x_min, x_max, num_grid_pts)
+	imag = np.linspace(y_min, y_max, num_grid_pts)
+	values = np.empty((num_grid_pts, num_grid_pts))
+	for i in range(num_grid_pts):
+		for j in range(num_grid_pts):
+			values[j, i] = _mandel_calc(real[i] + 1j*imag[j], tolerance)
+	_plotting(values, tolerance, x_min, x_max, y_min, y_max)
 	return real
 
 print("This code will compute the mandelbrot set for a user defined number of points between uer defined boundaries.\n")
@@ -45,21 +45,20 @@ print("The tolerance is the number of times a specific point will be interated o
 print("A plot is saved to file and then printed to screen.\n")
 print("Note that -2 -> x -> 2 & -2 -> y -> 2 encompass the set.\n")
 
+# Sample grids to run.
 # mandelbrot_main(-2, 2, -2, 2, 1000, 100)
 # mandelbrot_main(0, .5, .5, 1.0, 1000, 100)
 # mandelbrot_main(.5, .6, .3, .4, 1000, 100)
 # mandelbrot_main(.34, .36, .50, 5.2, 1000, 100
 # mandelbrot_main(.30, .32, .56, .58, 1000, 100)
 # mandelbrot_main(.36, .38, .58, .60, 1000, 50)
-# Global_x_min, Global_x_max, Global_y_min, Global_y_max = [float(x) for x in raw_input("Enter x & y plotting boundaries (xmin xmax ymin ymax): ").split()]
-# Global_Grid_size, Global_Tolerance = [int(x) for x in raw_input("Enter grid size and tolerance here (recommend 1000 100): ").split()]
 
-Global_x_min = float(input("Input x_min:"))
-Global_x_max = float(input("Input x_max:"))
-Global_y_min = float(input("Input y_min:"))
-Global_y_max = float(input("Input y_max:"))
-Global_Grid_size = int(input("Input grid_size:"))
-Global_Tolerance = int(input("Input tolerance:"))
+G_x_min = float(input("Input x_min:"))
+G_x_max = float(input("Input x_max:"))
+G_y_min = float(input("Input y_min:"))
+G_y_max = float(input("Input y_max:"))
+G_num_grid_pts = int(input("Input num_grid_pts:"))
+G_tolerance = int(input("Input tolerance:"))
 
 print("Inputs ok. Computing set ...")
-mandelbrot_main(Global_x_min, Global_x_max, Global_y_min, Global_y_max, Global_Grid_size, Global_Tolerance)
+_mandelbrot_main(G_x_min, G_x_max, G_y_min, G_y_max, G_num_grid_pts, G_tolerance)
